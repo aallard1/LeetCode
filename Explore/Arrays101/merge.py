a91895ea404b3db61ff00016a3595252cf259ae3
Alexandra <aallard170@gmail.com>
2021-06-30 13:29:27 -0700

    Added new problem solution

---
+++ Explore/Arrays101/merge.py
@@ -0,0 +1,21 @@
+def merge(nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        j = 0
+        for i in range(n + m):
+            if j == n:
+                break
+            if nums2[j] <= nums1[i] or (i - j) >= m:
+                nums1.insert(i, nums2[j])
+                nums1.pop()
+                j += 1
+
+        # j = 0
+        # while j < len(nums1):
+        #     for i in len(nums2):
+        #         if nums1[i] == 0:
+        #             nums1.insert(j, nums2[i])
+        #             j += 1
+        #             nums1.pop()
+        #         j += 1
