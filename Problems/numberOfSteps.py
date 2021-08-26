038e9a8c1e83b956e5bfdb113f8a843fa34b1f91
Alexandra <aallard170@gmail.com>
2021-08-26 10:28:13 -0700

    Simplified solution

--- Problems/numberOfSteps.py
+++ Problems/numberOfSteps.py
@@ -3,10 +3,9 @@ def numberOfSteps(num):
     while num > 0:
         if num % 2 == 0:
             num /= 2
-            count += 1
         elif num % 2 != 0:
             num -= 1
-            count += 1
+        count += 1
     return count
 
 numberOfSteps(14)
